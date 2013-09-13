56d942b5cf61548d2e3e434d671ef91492508476
Michael Ballard <michael@ballard.is>
2013-09-13 10:35:23 -0400

    issuer workflow

--- javascripts/backpack.js
+++ javascripts/backpack.js
@@ -1,3 +1,13 @@
+var fragment;
+
+if(window.location.hash) {
+  fragment = window.location.hash;
+} else {
+  fragment = 0;
+}
+
+console.log(fragment);
+
 $(document).ready(function() {
 
 var docroot = '/moz/backpack';
@@ -5,17 +15,21 @@ var docroot = '/moz/backpack';
 $.timeago.settings.allowFuture = true;
   //the click function for lists of badge thumbnails
   $( '.grid' ).delegate( "a", "click", function() {
-  	var target = $( this );
-  	var hashOrAction = $( this ).attr('class').split(' ')[1];
+    if($('.logged-out').length != 0) {
+      makeAlert('Please <a class="button small" href="persona.html">log in</a> to make changes to your badges.','alert');
+      return false;
+    }
+    var target = $( this );
+    var hashOrAction = $( this ).attr('class').split(' ')[1];
     var ui = 0;
     if (target.hasClass('badgethumb')) { ui = 'badge'; } 
     else if (target.hasClass('collectionthumb')) { ui = 'collection'; }
 
-  	//Display badge content and BadgeUI for clicked badge
-   	if (ui != 0) {
+    //Display badge content and BadgeUI for clicked badge
+    if (ui != 0) {
       //for square thumbnal badges
       if(target.parents('ul').hasClass('square')){
-  	    if (target.hasClass('chosen')) {
+        if (target.hasClass('chosen')) {
           $(target).find('.detail').animate({
             top: "160px"
           }, 400, "swing", function(){
@@ -23,14 +37,14 @@ $.timeago.settings.allowFuture = true;
               $(this).remove();
             });
           });
-  	    } else {
+        } else {
           $(target).find('.detail').animate({
             top: "0px"
           }, 400, "swing", function(){
             ui = makeUI(hashOrAction,ui)
             target.addClass('chosen').parents('li').append(ui).find('.ui').fadeIn('fast');
           });
-  	    }
+        }
       //for vertical thumbnail badges (in lists)
       } else if (target.parents('ul').hasClass('vertical')) {
         //SOMEB BUG HERE - WHEN YOU TOGGLE VISIBLE A BUNCH OF VERTICALS THEN TOGGLE INVISI ALL DISAPPEAR
@@ -47,32 +61,32 @@ $.timeago.settings.allowFuture = true;
       }
     return false;
   //Perform action based on clicked Badge UI item
-	} else if (target.hasClass('badge_action')) {
-		badgeAction(target);
-		if(!target.hasClass('bcol')) return false;
+  } else if (target.hasClass('badge_action')) {
+    badgeAction(target);
+    if(!target.hasClass('bcol')) return false;
   } else if (target.hasClass('collection_action')) {
     collectionAction(target);
   } else if (target.hasClass('toggle')) {
     //console.log(hashOrAction);
     $('#'+hashOrAction).fadeToggle();
     return false;
-	} else {
-		console.log('some other link');
-	}
+  } else {
+    console.log('some other link');
+  }
   });
 
   //a function to generate the dropdown BadgeUI from the clicked badge hash
   function makeUI(hash,what) {
-  	console.log('making a ' + what + ' ui for : ' + hash);
-  	var output = '' +
-  	'<div class="' + what + 'ui ui">' +
-  	'	<ul>';
+    console.log('making a ' + what + ' ui for : ' + hash);
+    var output = '' +
+    '<div class="' + what + 'ui ui">' +
+    ' <ul>';
 
       if(what == 'badge') {
         output += '' +
-      	'		<li><a class="badge_action bdel ' + hash + '" href="#">Delete</a></li>' +
-      	'		<li><a class="badge_action bcol ' + hash + '" data-dropdown="' + hash + '_coll_dd" href="#">Collections</a>' +
-      	'      <ul id="' + hash + '_coll_dd" class="f-dropdown" data-dropdown-content>' +
+        '   <li><a class="badge_action bdel ' + hash + '" href="#">Delete</a></li>' +
+        '   <li><a class="badge_action bcol ' + hash + '" data-dropdown="' + hash + '_coll_dd" href="#">Collections</a>' +
+        '      <ul id="' + hash + '_coll_dd" class="f-dropdown" data-dropdown-content>' +
                   getCollectionsByBadge(hash,'li') +
         '         <li class="divider"><hr></li>' +
         '         <li><a class="toggle ' + hash + '_batc_dd" href="#"><span class="title">Add to new or existing collection</span></a>' +
@@ -83,7 +97,7 @@ $.timeago.settings.allowFuture = true;
         '         </li>' +
         '      </ul>' +
         '   </li>' +
-        '		<li><a class="badge_action bdet ' + hash + '" href="#">Detail</a></li>';
+        '   <li><a class="badge_action bdet ' + hash + '" href="#">Detail</a></li>';
       } else {
       output += '' +
         '   <li><a class="collection_action cdel ' + hash + '" href="#">Delete</a></li>' +
@@ -92,23 +106,23 @@ $.timeago.settings.allowFuture = true;
       }
 
   output += '' +
-    '	</ul>' +
+    ' </ul>' +
     '</div>';
 
-  	return output;
+    return output;
 
   }
 
   //a function to process BadgeUI clicks (details,delete,etc.)
   function badgeAction(element) {
 
-	var action = element.attr('class').split(' ')[1];
-	var hash = element.attr('class').split(' ')[2];
+  var action = element.attr('class').split(' ')[1];
+  var hash = element.attr('class').split(' ')[2];
 
   console.log('action is : ' + action);
   console.log('target is : ' + hash);
 
-	if (action == 'bdel') {
+  if (action == 'bdel') {
     makeAlert('Are you sure you want to delete ' + $('.' + hash + ' .title').html() + '?','alert');
     } else if (action == 'bdet') {
       makeModal(element);
@@ -148,8 +162,8 @@ $.timeago.settings.allowFuture = true;
     if($('.alert-box').length != 0) {
       $('.alert-box').remove();
     }
-  	var alert = '<div data-alert class="alert-box ' + status + '"><span class="content">' + text + '</span><a href="#" class="close">&times;</a></div>';
-  	$(alert).prependTo($('body')).fadeIn('fast');
+    var alert = '<div data-alert class="alert-box ' + status + '"><span class="content">' + text + '</span><a href="#" class="close">&times;</a></div>';
+    $(alert).prependTo($('body')).fadeIn('fast');
   }
 
   //a function to get badge details and display them in a modal
@@ -172,12 +186,11 @@ $.timeago.settings.allowFuture = true;
     var height = firstli_h;
     var width = firstli_w;
 
-    if(numRows != 3) {
+    if(numRows == 4) {
       width = ((firstli_w * 2) + 20);
       height = ((firstli_h * 2) + 20);
       //display on the right if element is on the left
-      if(numRows == 1) { height*=2; width=firstli_w; }
-      if(elemPosition < (bodyWidth / 2) && (numRows == 4)) xpos = (xpos + width + 20);
+      if(elemPosition < (bodyWidth / 2)) xpos = (xpos + width + 20);
     } else if(numRows == 3){
       width = ((firstli_w * 1.5) + 20);
       height = ((firstli_h * 4) + 30);
@@ -189,7 +202,7 @@ $.timeago.settings.allowFuture = true;
   
     var details = retrieveBadge(element.attr('class').split(' ')[2]);
     var close = $('<a href="#" class="close">×</a>').click(function(){$('#badge_modal').remove();return false});
-    var inner = $('<div style="top:' + ypos + 'px;left:' + xpos + 'px;width:' + width + 'px;min-height:' + height + 'px;" id="badge_modal_inner"></div>');
+    var inner = $('<div style="top:' + ypos + 'px;left:' + xpos + 'px;width:' + width + 'px;height:' + height + 'px;" id="badge_modal_inner"></div>');
     var outer = $('<div id="badge_modal"></div>');
 
     outer.append(inner.append(details,close));
